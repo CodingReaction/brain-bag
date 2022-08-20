a7b6fc8b1f47c525bbdd1b34a9eb97721dd032e8
Max Texeira(codingreaction) <codingreaction@gmail.com>
2022-08-20 01:02:08 -0300

    Terms can be edited and also deleted (still requires redirect to form page)

--- bag_app/forms.py
+++ bag_app/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import Bag
+from .models import Bag, Term
 
 class BagForm(forms.Form):
     bag_name = forms.CharField(label="Name of the new BAG", max_length=100)
 
 
-class TermForm(forms.Form):
-    term_name = forms.CharField(label="Name of the new TERM", max_length=50)
-    term_url = forms.URLField(label="Link with info", max_length=200)
-    term_bag = forms.ModelChoiceField(queryset=Bag.objects.all(), empty_label=None)
+class TermForm(forms.ModelForm):
+    class Meta:
+        model = Term
+        fields = ("name", "url", "bag")
--- bag_app/urls.py
+++ bag_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.index, name="bags"),
     path("create/", views.create_bag, name="create"),
     path("create/term/", views.create_term, name="create-term"),
+    path("edit/term/<int:id>", views.edit_term, name="edit-term"),
+    path("delete/term/<int:id>", views.delete_term, name="delete-term"),
 ]
